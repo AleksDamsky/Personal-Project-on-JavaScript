3b0e7c41eab932a2a583af4a0b7271170ce47cf2
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-06-30 21:59:52 +0300

    js work, but him dont job

---
+++ JavaScript/index.js
@@ -0,0 +1,29 @@
+// new Parallax(document.querySelector('.parallax'));
+
+let getParallax = document.querySelector('.parallax');
+let layerDataDepth = Array.from(getParallax).map((getParallax) => {
+    getParallax.dataset.depth ? Number(getParallax.dataset.depth) : 0.2
+});
+
+startAnimation();
+
+function startAnimation() {
+    window.requestAnimationFrame(onAnimationFrame);
+};
+
+
+function onAnimationFrame() {
+    for(let i = 0; i <= getParallax.length; i++){
+        const layer = getParallax[i];
+        const layerDepth = layerDataDepth[i];
+        const y0ffset = -window.scrollY * layerDepth;
+        setPosition(layer, y0ffset);
+    }
+
+    window.requestAnimationFrame(onAnimationFrame);
+};
+
+function setPosition(element, yPosition) {
+    element.style.transform = `translate(0, ${yPosition.toFixed(1)}px, 0)`;
+
+}
---
+++ JavaScript/parallax.js
@@ -0,0 +1,5 @@
+class Parallax {
+    constructor(elements, options = {}) {
+        console.log(elements);
+    }
+}
